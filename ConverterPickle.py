ec4fc356181e736310e0b913299ff4bd011d855f
BM683 <132838948+BM683@users.noreply.github.com>
2023-05-08 18:07:38 +0100

    Add files via upload

---
+++ ConverterPickle.py
@@ -0,0 +1,140 @@
+
+# This code is based on a GitHub repository that served as a reference during development.
+# Repo URL: https://github.com/ABzry/battery_skunkworks.git 
+
+# Required libraries
+from scipy.io import loadmat, whosmat
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import pickle
+import os
+
+
+# Function to build dictionaries for discharge, charge, and impedance data
+def build_dictionaries(mess):
+
+    discharge, charge, impedance = {}, {}, {}
+
+    # Iterate through elements in mess and assign data to discharge, charge, or impedance dictionaries
+    for i, element in enumerate(mess):
+
+        step = element[0][0]
+
+        # Discharge data
+        if step == 'discharge':
+            discharge[str(i)] = {}
+            year = int(element[2][0][0])
+            month = int(element[2][0][1])
+            day = int(element[2][0][2])
+            hour = int(element[2][0][3])
+            minute = int(element[2][0][4])
+            second = int(element[2][0][5])
+            millisecond = int((second % 1)*1000)
+            date_time = datetime.datetime(year, month, day, hour, minute, second, millisecond)        
+
+            discharge[str(i)]["date_time"] = date_time.strftime("%d %b %Y, %H:%M:%S")
+
+            data = element[3]
+
+            discharge[str(i)]["voltage_battery"] = data[0][0][0][0].tolist()
+            discharge[str(i)]["current_battery"] = data[0][0][1][0].tolist()
+            discharge[str(i)]["temp_battery"] = data[0][0][2][0].tolist()
+            discharge[str(i)]["current_load"] = data[0][0][3][0].tolist()
+            discharge[str(i)]["voltage_load"] = data[0][0][4][0].tolist()
+            discharge[str(i)]["time"] = data[0][0][5][0].tolist()
+            discharge[str(i)]["capacity"] = data[0][0][6][0].tolist()
+
+        # Charge data
+        if step == 'charge':
+            charge[str(i)] = {}
+            #charge[str(i)]["amb_temp"] = str(element[1][0][0])
+            year = int(element[2][0][0])
+            month = int(element[2][0][1])
+            day = int(element[2][0][2])
+            hour = int(element[2][0][3])
+            minute = int(element[2][0][4])
+            second = int(element[2][0][5])
+            millisecond = int((second % 1)*1000)
+            date_time = datetime.datetime(year, month, day, hour, minute, second, millisecond)        
+
+            charge[str(i)]["date_time"] = date_time.strftime("%d %b %Y, %H:%M:%S")
+
+            data = element[3]
+
+            charge[str(i)]["voltage_battery"] = data[0][0][0][0].tolist()
+            charge[str(i)]["current_battery"] = data[0][0][1][0].tolist()
+            charge[str(i)]["temp_battery"] = data[0][0][2][0].tolist()
+            charge[str(i)]["current_load"] = data[0][0][3][0].tolist()
+            charge[str(i)]["voltage_load"] = data[0][0][4][0].tolist()
+            charge[str(i)]["time"] = data[0][0][5][0].tolist()
+
+        # Impedance data
+        if step == 'impedance':
+            impedance[str(i)] = {}
+            #impedance[str(i)]["amb_temp"] = str(element[1][0][0])
+            year = int(element[2][0][0])
+            month = int(element[2][0][1])
+            day = int(element[2][0][2])
+            hour = int(element[2][0][3])
+            minute = int(element[2][0][4])
+            second = int(element[2][0][5])
+            millisecond = int((second % 1)*1000)
+            date_time = datetime.datetime(year, month, day, hour, minute, second, millisecond)        
+
+            impedance[str(i)]["date_time"] = date_time.strftime("%d %b %Y, %H:%M:%S")
+
+            data = element[3]
+
+            impedance[str(i)]["sense_current"] = {}
+            impedance[str(i)]["battery_current"] = {}
+            impedance[str(i)]["current_ratio"] = {}
+            impedance[str(i)]["battery_impedance"] = {}
+            impedance[str(i)]["rectified_impedance"] = {}
+
+            impedance[str(i)]["sense_current"]["real"] = np.real(data[0][0][0][0]).tolist()
+            impedance[str(i)]["sense_current"]["imag"] = np.imag(data[0][0][0][0]).tolist()
+
+            impedance[str(i)]["battery_current"]["real"] = np.real(data[0][0][1][0]).tolist()
+            impedance[str(i)]["battery_current"]["imag"] = np.imag(data[0][0][1][0]).tolist()
+
+            impedance[str(i)]["current_ratio"]["real"] = np.real(data[0][0][2][0]).tolist()
+            impedance[str(i)]["current_ratio"]["imag"] = np.imag(data[0][0][2][0]).tolist()
+
+            impedance[str(i)]["battery_impedance"]["real"] = np.real(data[0][0][3]).tolist()
+            impedance[str(i)]["battery_impedance"]["imag"] = np.imag(data[0][0][3]).tolist()
+
+            impedance[str(i)]["rectified_impedance"]["real"] = np.real(data[0][0][4]).tolist()
+            impedance[str(i)]["rectified_impedance"]["imag"] = np.imag(data[0][0][4]).tolist()
+
+            impedance[str(i)]["re"] = float(data[0][0][5][0][0])
+            impedance[str(i)]["rct"] = float(data[0][0][6][0][0])
+            
+    return discharge, charge, impedance
+
+# Function to save dictionary as a pickle file
+def save_pickle(dictionary, name):
+    with open(name + '.pickle', 'wb') as f:
+        pickle.dump(dictionary, f)
+        
+
+# Get the current directory path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Find all .mat files in the current directory
+filenames = [f for f in os.listdir(current_dir) if f.endswith('.mat')]
+
+# Iterate through each .mat file, load its data, build dictionaries, and save as pickle files
+for filename in filenames:
+    name = filename.split('.mat')[0]
+    print(name)
+    struct = loadmat(current_dir + '/' + filename)
+    mess = struct[name][0][0][0][0]
+    
+    discharge, charge, impedance = build_dictionaries(mess)
+    
+    save_pickle(discharge, name + '_discharge')
+    save_pickle(charge, name + '_charge')    
+    save_pickle(impedance, name + '_impedance')    
+    
+    
